16b48f7872472a208bbd79932be6c3786c484fe1
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-26 21:37:50 -0600

    Ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Ejercicio 2
+
+Console.WriteLine(@" Utilizando la constante matemática de E: Math.E conocido en ocasiones como
+número de Euler o constante de Napier, utilizar ese valor para hacer los siguientes
+cálculos utilizando las funciones en C# y mostrarlas en consola.
+
+Redondear los valores decimales del número hasta la 10 posición");
+
+double e = Math.E;
+
+Console.WriteLine($"\nEl valor de e redondeado a la decima posicion es: {Math.Round(e, 10)}");
