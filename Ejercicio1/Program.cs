7803ee857ca5926d6d158863e54bfa22add6fef6
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-26 20:15:12 -0600

    Ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Ejercicio 1
+
+Console.WriteLine(@"Utilizando la constante matemática de PI: Math.PI utilizar ese valor para hacer los
+siguientes cálculos utilizando las funciones en C# y mostrarlas en consola.
+1- Truncar el valor de PI
+2- Redondear hacia arriba
+3- Redondear hacia abajo");
+
+double Pi = Math.PI;
+
+Console.WriteLine($"\nValor de PI truncado: {Math.Truncate(Pi)}");
+
+Console.WriteLine($"Valor de PI redondeado hacia arriba: {Math.Ceiling(Pi)}");
+
+Console.WriteLine($"Valor de PI redondeado hacia abajo: {Math.Floor(Pi)}");
