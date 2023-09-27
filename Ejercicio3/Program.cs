a79b7b4c5249ad1bd1ca8a69868bb8782274d54f
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-26 21:54:03 -0600

    //Ejercicio 3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ejercicio 3
+
+Console.WriteLine(@"Realice un programa que calcule la suma acumulada 
+de los cuadrados de los primeros 20 dígitos.
+
+Ejemplo suma = (1x1) + (2x2) + (3x3) + .. (18*18) + (19x19) + (20*20)");
+
+
+var Num = Enumerable.Range(1, 20);
+
+double Sum = Num.Sum(Num => Math.Pow(Num, 2));
+
+Console.WriteLine($"\nLa suma acumulada de los cuadrados de los primeros 20 números es: {Sum}");
